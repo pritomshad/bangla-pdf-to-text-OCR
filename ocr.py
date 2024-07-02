3b7068e6844f934f0c0c5d9fd70b046508e097aa
prit0m <59039346+pritomshad@users.noreply.github.com>
2024-07-02 21:36:28 +0600

    PY script added

---
+++ ocr.py
@@ -0,0 +1,23 @@
+import os
+import pytesseract
+from PIL import Image
+
+# Set TESSDATA_PREFIX environment variable in the script
+os.environ['TESSDATA_PREFIX'] = r'C:\Program Files\Tesseract-OCR\tessdata'
+# Path to your Tesseract executable
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+
+full_book = ''
+pages = int(input("Page numbers in your book: "))
+filetype = input("File type of the images (i.e. jpg, png): ")
+
+for i in range(1,pages+1):
+    image = Image.open(str(i) + '_1.' + filetype)
+    text = pytesseract.image_to_string(image, lang='ben')
+    full_book = full_book + '\n' + text
+    print("page "+str(i)+" done.")
+
+
+# Write the extracted text to a text file
+with open('Syed Mujtaba Ali Rachanaboli 3.txt', 'w', encoding='utf-8') as file:
+    file.write(full_book)
